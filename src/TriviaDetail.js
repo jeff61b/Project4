8aab16fc611cb216635546c6d94d5d52331db032
hx08371 <brammannjeffreyt@johndeere.com>
2020-09-14 23:14:28 -0500

    adding more functionto the trivia detail page

--- src/TriviaDetail.js
+++ src/TriviaDetail.js
@@ -13,21 +13,21 @@ class TriviaDetail extends Component {
     const triviaDetail = this.props.triviaQuestions.find((question) => {
       return question.id == this.props.match.params.id;
     });
-    const triviaAnswer1 = this.props.triviaQuestions.find((answer1) => {
-      return answer1.id == this.props.match.params.id;
-    });
+    // const triviaAnswer1 = this.props.triviaQuestions.find((answer1) => {
+    //   return answer1.id == this.props.match.params.id;
+    // });
     console.log(triviaDetail);
-    console.log(triviaAnswer1);
     return (
       <div>
         <h1>Trivia Detail Page</h1>
         <h2>Question: {triviaDetail.question}</h2>
-        <h3>Answer 1: {triviaDetail.answer1}</h3>
-        <h3>Answer 2: {triviaDetail.answer2}</h3>
-        <h3>Answer 3: {triviaDetail.answer3}</h3>
-        <h3>Answer 4: {triviaDetail.answer4}</h3>
-        <h3>Correct Answer: {triviaDetail.correctAnswer}</h3>
-        <h3>Category: {triviaDetail.categoryId}</h3>
+        <h2>Answer 1: {triviaDetail.answer1}</h2>
+        <h2>Answer 2: {triviaDetail.answer2}</h2>
+        <h2>Answer 3: {triviaDetail.answer3}</h2>
+        <h2>Answer 4: {triviaDetail.answer4}</h2>
+        <h2>Correct Answer: {triviaDetail.correctAnswer}</h2>
+        {/* <h3>Category: {triviaDetail.categoryId}</h3> */}
+        <h2>Trivia Category: {triviaDetail.Category.name}</h2>
 
         {/* <h5>Add a New Song</h5>
         <form onSubmit={this.props.addSong}>
